67d118385871e4baa0c9ab6b3ede6c1102b7ebe6
Chris Caballero <chrismcaballero@gmail.com>
2023-08-15 10:27:25 -0400

    Update utils documentation and readability

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -2,22 +2,41 @@ import numpy as np
 import pandas as pd
 
 def get_data(filename):
+    """
+    Load data from a JSON file and convert it into a Pandas DataFrame.
+
+    Args:
+        filename (str): Name of the JSON file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the loaded data.
+    """
     import json
 
     print('Loading data...')
     with open(filename) as f:
         data = json.load(f)
         df = pd.json_normalize(data)
-        df.columns = ['index', 'type', 'id', 'score', 'tags', 'zip_code','complaint_id', 'issue', 'date_received',
-                  'state', 'consumer_disputed', 'product','company_response', 'company', 'submitted_via',
-                  'date_sent_to_company', 'company_public_response','sub_product', 'timely',
-                  'complaint', 'sub_issue','consent_provided']
+        df.columns = ['index', 'type', 'id', 'score', 'tags', 'zip_code', 'complaint_id', 'issue', 'date_received',
+                      'state', 'consumer_disputed', 'product', 'company_response', 'company', 'submitted_via',
+                      'date_sent_to_company', 'company_public_response', 'sub_product', 'timely',
+                      'complaint', 'sub_issue', 'consent_provided']
         df[df['complaint'] == ''] = np.nan
         df.dropna(subset=['complaint'], inplace=True)
     
     return df
 
 def clean(text):
+    """
+    Clean and preprocess text by converting to lowercase, removing non-alphanumeric characters,
+    digits, and common placeholders.
+
+    Args:
+        text (str): Input text.
+
+    Returns:
+        str: Cleaned text.
+    """
     import re
 
     text = text.lower()
@@ -28,17 +47,45 @@ def clean(text):
     return text
 
 def lemmatize(text):
+    """
+    Lemmatize text using spaCy.
+
+    Args:
+        text (str): Input text.
+
+    Returns:
+        str: Lemmatized text.
+    """
     import en_core_web_sm
     nlp = en_core_web_sm.load()
     doc = nlp(text)
     return ' '.join(tok.lemma_ for tok in doc)
 
-def noun_extraction(text): 
+def noun_extraction(text):
+    """
+    Extract nouns from text using TextBlob.
+
+    Args:
+        text (str): Input text.
+
+    Returns:
+        str: Extracted nouns.
+    """
     from textblob import TextBlob
     blob = TextBlob(text)
     return ' '.join(tok for (tok, tag) in blob.tags if tag == 'NN')
 
-def preprocess(df, fname = None):
+def preprocess(df, fname=None):
+    """
+    Preprocess the complaints data.
+
+    Args:
+        df (pd.DataFrame): Original DataFrame containing complaints data.
+        fname (str, optional): File name to save preprocessed data.
+
+    Returns:
+        pd.DataFrame: Preprocessed DataFrame containing cleaned, lemmatized, and noun-extracted text.
+    """
     print('Cleaning Text...')
     complaints = pd.DataFrame(df['complaint'].apply(clean))
     print('Sample:', complaints['complaint'].sample(1))
@@ -59,10 +106,22 @@ def preprocess(df, fname = None):
     return df, complaints
 
 def word_frequencies(text, pipeline=None, n=1):
+    """
+    Calculate word frequencies in the text.
+
+    Args:
+        text (pd.Series): Series of text data.
+        pipeline (sklearn.pipeline.Pipeline, optional): Text processing pipeline.
+        n (int, optional): N-gram range for word frequencies.
+
+    Returns:
+        list: List of tuples containing word-frequency pairs.
+        scipy.sparse.csr_matrix: Document-term matrix.
+    """
     from sklearn.feature_extraction.text import CountVectorizer
 
     if pipeline is None:
-        vectorizer = CountVectorizer(ngram_range=(n,n))
+        vectorizer = CountVectorizer(ngram_range=(n, n))
         X = vectorizer.fit_transform(text)
     else:
         vectorizer = pipeline['count']
@@ -70,11 +129,20 @@ def word_frequencies(text, pipeline=None, n=1):
         
     frequency = X.sum(axis=0)
     word_frequency = [(word, frequency[0, vectorizer.vocabulary_[word]]) for word in vectorizer.vocabulary_]
-    word_frequency = sorted(word_frequency, key = lambda x: x[1], reverse=True)
+    word_frequency = sorted(word_frequency, key=lambda x: x[1], reverse=True)
     
     return word_frequency, X
 
 def show_top_words(text=None, pipeline=None, word_frequency=None, num_samples=10):
+    """
+    Display the top words by frequency in a bar plot.
+
+    Args:
+        text (pd.Series, optional): Series of text data.
+        pipeline (sklearn.pipeline.Pipeline, optional): Text processing pipeline.
+        word_frequency (list, optional): List of tuples containing word-frequency pairs.
+        num_samples (int, optional): Number of top words to display.
+    """
     import seaborn as sns
     import matplotlib.pyplot as plt
 
@@ -84,7 +152,7 @@ def show_top_words(text=None, pipeline=None, word_frequency=None, num_samples=10
             return
         word_frequency, _ = word_frequencies(text, pipeline=pipeline, n=1)
         
-    plt.figure(figsize=[10,4])
+    plt.figure(figsize=[10, 4])
     top_words = word_frequency[:num_samples]
     items = [x[0] for x in top_words]
     counts = [x[1] for x in top_words]
@@ -94,9 +162,20 @@ def show_top_words(text=None, pipeline=None, word_frequency=None, num_samples=10
     plt.xlabel("Word")
     plt.ylabel("Frequency")
     plt.title(f"Top {num_samples} words by frequency")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
     plt.show()
 
 def vectorizer_pipeline(text):
+    """
+    Create a text vectorization pipeline.
+
+    Args:
+        text (pd.Series): Series of text data.
+
+    Returns:
+        sklearn.pipeline.Pipeline: Text vectorization pipeline.
+    """
     from sklearn.pipeline import Pipeline
     from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
     from sklearn.decomposition import NMF
